dc9359f567a5e14144c7e49840f25fc8460fa99c
ankit1996A <ankit.sharma@pragmaapps.com>
2026-02-28 07:30:53 +0530

    Added base node and integrated.

--- frontend/src/nodes/textNode.js
+++ frontend/src/nodes/textNode.js
@@ -1,35 +1,26 @@
 // textNode.js
 
 import { useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import { Position } from 'reactflow';
+import { BaseNode } from './BaseNode';
 
 export const TextNode = ({ id, data }) => {
   const [currText, setCurrText] = useState(data?.text || '{{input}}');
 
-  const handleTextChange = (e) => {
-    setCurrText(e.target.value);
-  };
+  const handles = [
+    { type: 'source', position: Position.Right, id: `${id}-output` },
+  ];
 
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
-      <div>
-        <span>Text</span>
-      </div>
-      <div>
-        <label>
-          Text:
-          <input 
-            type="text" 
-            value={currText} 
-            onChange={handleTextChange} 
-          />
-        </label>
-      </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id={`${id}-output`}
-      />
-    </div>
+    <BaseNode title="Text" handles={handles}>
+      <label>
+        Text:
+        <input
+          type="text"
+          value={currText}
+          onChange={(e) => setCurrText(e.target.value)}
+        />
+      </label>
+    </BaseNode>
   );
-}
+};
